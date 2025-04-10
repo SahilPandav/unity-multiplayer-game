9d7dc37052f5f514297c42effcb4bf8ecd9363f2
Meet <161622011+ghogharimeet21@users.noreply.github.com>
2025-04-10 15:50:44 +0530

    added to git

---
+++ Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public class LeaderboardEntityDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text displayText;
+
+    private FixedString32Bytes displayName;
+
+    public int TeamIndex { get; private set; }
+    public ulong ClientId { get; private set; }
+    public int Coins { get; private set; }
+
+    public void Initialise(ulong clientId, FixedString32Bytes displayName, int coins)
+    {
+        ClientId = clientId;
+        this.displayName = displayName;
+
+        UpdateCoins(coins);
+    }
+
+    public void Initialise(int teamIndex, FixedString32Bytes displayName, int coins)
+    {
+        TeamIndex = teamIndex;
+        this.displayName = displayName;
+
+        UpdateCoins(coins);
+    }
+
+    public void SetColour(Color colour)
+    {
+        displayText.color = colour;
+    }
+
+    public void UpdateCoins(int coins)
+    {
+        Coins = coins;
+
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        displayText.text = $"{transform.GetSiblingIndex() + 1}. {displayName} ({Coins})";
+    }
+}
